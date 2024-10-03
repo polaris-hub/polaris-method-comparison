07f31e07c0ff09904cba3d3b1de41a30d3c50e53
Jeremy Ash <jeremyryanash@gmail.com>
2024-10-03 19:09:11 -0400

    Use new CV code in model building

---
+++ ADME_example/model_building/chemprop_wrapper.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import os
+import tempfile
+import subprocess
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import chemprop
+import shutil
+
+class ChemPropWrapper:
+    def __init__(self, y_name):
+        self.y_name = y_name
+
+    def validate(self, train, test):
+        with tempfile.TemporaryDirectory() as temp_dirname:
+        
+            st_cols = ["SMILES", "Name", "Sol"]
+
+            train['Sol_bins'] = pd.cut(train['Sol'], bins=10, labels=False)
+            train, val = train_test_split(train, test_size=0.1, stratify=train['Sol_bins'], random_state=42)
+
+            # Write the input files
+            train[st_cols].to_csv(f"{temp_dirname}/train.csv", index=False)
+            val[st_cols].to_csv(f"{temp_dirname}/val.csv", index=False)
+            test[st_cols].to_csv(f"{temp_dirname}/test.csv", index=False)
+
+            # Set up ChemProp arguments for training
+            data_path = f"{temp_dirname}/train.csv"
+            separate_val_path = f"{temp_dirname}/val.csv"
+            separate_test_path = f"{temp_dirname}/test.csv"
+            save_dir = f"{temp_dirname}/result"
+
+            if os.path.exists(save_dir):
+                shutil.rmtree(save_dir, ignore_errors=True)
+
+            arguments = [
+                '--data_path', data_path,
+                '--separate_test_path', separate_test_path,
+                '--separate_val_path', separate_val_path,
+                '--num_folds', '1',
+                '--epochs', '30',
+                '--ensemble_size', '10',
+                '--ignore_columns', 'Name',
+                '--smiles_columns', 'SMILES',
+                '--quiet',
+                '--dataset_type', 'regression',
+                '--save_dir', save_dir,
+                '--save_preds',
+                '--gpu', '0'
+            ]
+            args = chemprop.args.TrainArgs().parse_args(arguments)
+
+            # Train the model
+            mean_score, std_score = chemprop.train.cross_validate(args=args, train_func=chemprop.train.run_training)
+
+            # Make model predictions
+            predict_args = [
+                '--test_path', separate_test_path,
+                '--preds_path', f"{temp_dirname}/pred.csv",
+                '--checkpoint_dir', save_dir,
+                '--smiles_columns', 'SMILES',
+                '--gpu', '0'
+            ]
+            predict_args = chemprop.args.PredictArgs().parse_args(predict_args)
+            chemprop.train.make_predictions(args=predict_args)
+
+            # Read the results
+            result_df = pd.read_csv(f"{temp_dirname}/pred.csv")
+            return result_df['Sol'].values
+
+def main():
+    df = pd.read_csv("https://raw.githubusercontent.com/PatWalters/datafiles/refs/heads/main/biogen_logS.csv")
+    train, test = train_test_split(df)
+    chemprop_wrapper = ChemPropWrapper("logS")
+    pred = chemprop_wrapper.validate(train, test)
+    print(pred)
+
+if __name__ == "__main__":
+    main()
---
+++ ADME_example/model_building/run_benchmark.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from chemprop_wrapper import ChemPropWrapper
+from chemprop_wrapper_multitask import ChemPropMultitaskWrapper
+from lgbm_wrapper import LGBMMorganCountWrapper, LGBMPropWrapper
+import useful_rdkit_utils as uru
+import pandas as pd
+
+def main():
+    dataset_name = "sol_processed.csv"
+    ds = pd.read_csv(dataset_name)
+    y = "Sol" 
+    # get the dataframe from the Polaris dataset
+    df = ds.dropna(subset=y).copy()
+    df.rename(columns={"smiles" : "SMILES"},inplace=True)
+    print(f"Processing {y} with {len(df)} records")
+    model_list = [("chemprop_st",ChemPropWrapper),
+                  ("chemprop_mt",ChemPropMultitaskWrapper),
+                  ("lgbm_morgan", LGBMMorganCountWrapper)]
+    group_list = [("random", uru.get_random_clusters),
+                  ("butina", uru.get_butina_clusters),
+                  ("scaffold", uru.get_bemis_murcko_clusters)]
+    result_df = uru.cross_validate(df,model_list,y,group_list)
+    result_df.to_csv(f"{y}_results.csv",index=False)
+
+
+if __name__ == "__main__":
+    main()
